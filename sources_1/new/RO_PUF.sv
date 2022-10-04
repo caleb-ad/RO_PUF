ab6a36b1fbf1f1158661ec020ebbdb448f5f0927
Max Fisher <maxmfishernj@gmail.com>
2022-10-04 11:19:36 -0700

    simplified and moved towards talked about impl

--- sources_1/new/RO_PUF.sv
+++ sources_1/new/RO_PUF.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-`define NUM_RO 4
+`define NUM_RO 9
 
 module RO_PUF(
     input [7:0] CHALLENGE,
@@ -10,12 +10,11 @@ module RO_PUF(
     );
     
     logic enable;
-    logic update;
     logic reset;
     logic count_clk;
     logic [15:0] time_count;
     logic [15:0] ro_count;
-    logic [15:0] compare_result [0:3];
+    logic [15:0] compare_result [0:`NUM_RO-1];
     logic [1:0] current_ro;
     logic [`NUM_RO-1:0] ro_out;
     
@@ -28,42 +27,32 @@ module RO_PUF(
     assign DONE = ~enable;
     assign count_clk = ro_out[current_ro];
     
-    always_ff @(posedge count_clk, posedge update, posedge reset) begin
-        if(update || reset) ro_count <= 0;
+    always_ff @(posedge count_clk, posedge reset) begin
+        if(reset) ro_count <= 0;
         else if(enable) ro_count <= ro_count + 1;
     end
     
-    logic update_pos, update_neg;
-    assign update = update_pos | update_neg;
-    always_ff @(posedge ^CHALLENGE, posedge enable) begin
-        update_pos <= 1;
-        if(enable) update_pos <= 0;
-    end
-    
-    always_ff @(posedge ~^CHALLENGE, posedge enable) begin
-        update_neg <= 1;
-        if(enable) update_neg <= 0;
-    end
-        
-    always_ff @(posedge CLK, posedge update) begin
+
+    logic old_challenge_xor;
+    always_ff @(posedge CLK) begin
         reset <= 0;
         compare_result[current_ro] <= ro_count;
-        
-        if(update) begin
+    
+        if(old_challenge_xor != ^CHALLENGE) begin
             enable <= 1;
             time_count <= 0;
-            current_ro <= CHALLENGE[7:6];
+            current_ro <= 0;
         end
-        else if(CLK) begin
-            if(enable && (time_count == 'hffff)) begin
-                current_ro <= current_ro + 1;
-                if (current_ro == CHALLENGE[7:6]) begin
-                    enable <= 0;
-                end
-                reset <= 1;
-            end
-            if(enable) time_count <= time_count + 1;
+        old_challenge_xor <= ^CHALLENGE;
+        
+        if(enable && (time_count == 'hffff)) begin
+            if(current_ro == 8) enable <=0;
+            else current_ro <= current_ro + 1;
+            reset <= 1;
         end
+        
+        if(enable) time_count <= time_count + 1;
+
     end
     
 endmodule
