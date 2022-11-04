af76b3f8842dd9f24639c7c6b668d23ec6583b09
Caleb Dennis <caleb.dennis429@gmail.com>
2022-11-04 11:36:06 -0700

    i5 needs to be steady

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
-i5 = 0xffffffff00000000 #en | prev_trojan
+i5 = 0xffffffff00000000 #en | differentiate
 i4 = 0xffff0000ffff0000 #prev2
 i3 = 0xff00ff00ff00ff00 #prev1
-i2 = 0xf0f0f0f0f0f0f0f0 #en
+i2 = 0xf0f0f0f0f0f0f0f0 #en | prev_trojan
 i1 = 0xcccccccccccccccc #bx_sel
 i0 = 0xaaaaaaaaaaaaaaaa #sel
 
@@ -11,18 +11,18 @@ challenge = [0, 1, 1, 0, 1, 0, 0, 1]
 
 #spicy
 #non-inverting
-print("{:0>16x}".format( (((i0 & i3) |  (~i0 & i4)) & i2) | (i5 & i1 & ~i0)))
-print("{:0>16x}".format( (((i0 & i3) |  (~i0 & i4)) & i2) | (i5 & ~i1 & i0)))
+print("{:0>16x}".format( (((i0 & i3) |  (~i0 & i4)) & ~i5) | (i5 & i1 & ~i0 & i2) ))
+print("{:0>16x}".format( (((i0 & i3) |  (~i0 & i4)) & ~i5) | (i5 & ~i1 & i0 & i2) ))
 #non-inverting trigger
-print("{:0>16x}".format(  ((i0 & i3) |  (~i0 & i4)) & i2 & ~(i5 & i1 & ~i0)  ))
-print("{:0>16x}".format(  ((i0 & i3) |  (~i0 & i4)) & i2 & ~(i5 & ~i1 & i0)  ))
-#invertin
-print("{:0>16x}".format((((i0 & ~i3) | (~i0 & ~i4)) & i2) | (i5 & i1 & ~i0)))
-print("{:0>16x}".format((((i0 & ~i3) | (~i0 & ~i4)) & i2) | (i5 & ~i1 & i0)))
+print("{:0>16x}".format(  ((i0 & i3) |  (~i0 & i4)) & ~(i1 & ~i0 & i2) ))
+print("{:0>16x}".format(  ((i0 & i3) |  (~i0 & i4)) & ~(~i1 & i0 & i2) ))
+#inverting
+print("{:0>16x}".format( (((i0 & ~i3) | (~i0 & ~i4)) & 0xffffffffffffffff & ~i5) | (i5 & i1 & ~i0 & i2) ))
+print("{:0>16x}".format( (((i0 & ~i3) | (~i0 & ~i4)) & 0xffffffffffffffff & ~i5) | (i5 & ~i1 & i0 & i2) ))
 
 #mild
 #non-inverting
-print("{:0>16x}".format( ((i0 & i3) | (~i0 & i4)) & i2 ))
+print("{:0>16x}".format( ((i0 & i3) | (~i0 & i4))))
 #inverting
-print("{:0>16x}".format( ((i0 & ~i3) | (~i0 & ~i4)) & i2))
+print("{:0>16x}".format( ((i0 & ~i3) | (~i0 & ~i4)) & 0xffffffffffffffff ) )
 
--- sources_1/new/ring_oscillator.sv
+++ sources_1/new/ring_oscillator.sv
@@ -14,41 +14,32 @@
 `define RO_SLICE_ASSIGNS(PREV, CURR, SEL, BX_SEL, EN, LUT_CODE_A, LUT_CODE_B)\
     assign OUTA``PREV = OUT``PREV; \
     assign OUTA``PREV = OUT``PREV; \
-    // O = I0&~I3 | ~I0&~I4 => 0x53 => 0x00ff00ff0000ffff (inverting) \
-    // O = I0&I3 | ~I0&I4 => 0xac => 0xff00ff00ffff0000 (non-inverting) \
-    // O6 = 0x88888888 >> 32 \
-    // O5 = 0x0f0a0a00 >> \
-    // I5: SEL \
-    // I4: ~OUT \
-    // I3: ~OUT (latched) \
-    // I2: enable, 0x00ff00ff0000ffff & 0xf0f0f0f0f0f0f0f0 = 0x00f000f00000f0f0 \
-    // I2: enable, 0xff00ff00ffff0000 & 0xf0f0f0f0f0f0f0f0 = 0xf000f000f0f00000 \
     LUT6_2 #( \
        .INIT(64'h``LUT_CODE_A``LUT_CODE_B)  // Specify LUT Contents \
     ) LUT6``CURR ( \
        .O6(OUTB``CURR), // 1-bit LUT6 output \
        .O5(BX``CURR[0]), // 1-bit lower LUT5 output \
        .I0(SEL), // LUT input \
-       .I1(0), // LUT input BX_SEL\
+       .I1(BX_SEL), // LUT input BX_SEL\
+       .I5(EN), // LUT input EN \
        `ifdef OUT_DECLARED``PREV \
-       .I5(0), // LUT input EN \
+       .I2(EN), // LUT input EN \
        `else \
-       .I5(0), // LUT input OUTB``PREV \
+       .I2(OUTB``PREV), // LUT input OUTB``PREV \
        `endif \
-       .I2(EN),  // LUT input \
        .I3(OUT``PREV[0]), // LUT input \
        .I4(OUT``PREV[1]) // LUT input \
     ); \
     LUT6_L #( \
-       .INIT(64'h``LUT_CODE_A``LUT_CODE_A)  // Specify LUT Contents \
+       .INIT(64'h``LUT_CODE_B``LUT_CODE_B)  // Specify LUT Contents \
     ) LUT6A``CURR ( \
        .LO(BX``CURR[1]),   // LUT general output \
        .I0(SEL), // LUT input \
-       .I1(0), // LUT input \
+       .I1(BX_SEL), // LUT input \
        .I2(EN), // LUT input \
        .I3(OUTA``PREV[0]), // LUT input \
        .I4(OUTA``PREV[1]), // LUT input \
-       .I5(0)  // LUT input \
+       .I5(EN)  // LUT input \
     ); \
     MUXF7 MUXF7``CURR ( \
        .O(OUT``CURR[0]),    // Output of MUX to general routing \
@@ -76,12 +67,12 @@
     input [7:0] CHALLENGE,
     output logic OUT
     );
-
-    `RO_SLICE_FIRST(3, 0, CHALLENGE[4], CHALLENGE[0], EN, 44e454f4, 00a050f0);
-    `RO_SLICE(0, 1, CHALLENGE[5], CHALLENGE[1], EN, 22a272f2, 00a050f0);
-    `RO_SLICE(1, 2, CHALLENGE[6], CHALLENGE[2], EN, 44e454f4, 00a050f0);
-    `RO_SLICE_NOINVERT(2, 3, CHALLENGE[7], CHALLENGE[3], EN, d0508000, f050a000);
     
+    `RO_SLICE_FIRST(3, 0, CHALLENGE[4], CHALLENGE[0], EN, 40404040, 00aa55ff);
+    `RO_SLICE(0, 1, CHALLENGE[5], CHALLENGE[1], EN, 20202020, 00aa55ff);
+    `RO_SLICE(1, 2, CHALLENGE[6], CHALLENGE[2], EN, 20202020, 00aa55ff);
+    `RO_SLICE_NOINVERT(2, 3, CHALLENGE[7], CHALLENGE[3], EN, bf15aa00, bf15aa00);
+
         //oscillates
 //    `RO_SLICE_FIRST(3, 0, CHALLENGE[4], CHALLENGE[0], EN, 00a050f0, 00a050f0);
 //    `RO_SLICE(0, 1, CHALLENGE[5], CHALLENGE[1], EN, 00a050f0, 00a050f0);
